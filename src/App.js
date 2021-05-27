9790f972ad48f352e4570148a4d0c68327d499c9
vilmaviola <vilma.ahlholm@live.se>
2021-05-27 08:56:16 +0200

    test changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vilmas github page!
         </p>
         <a
           className="App-link"
